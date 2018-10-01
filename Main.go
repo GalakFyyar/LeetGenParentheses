a18094708e4af1b0c8474ef3e29aff48d9842dfd
InTheCrunch <gcrltn@gmail.com>
2018-10-01 03:15:39 -0400

    added noAppend

--- Main.go
+++ Main.go
@@ -8,27 +8,64 @@ import (
 var mapOfResults map[int][]string
 
 func main() {
-	n := 17
+	n := 16
 
 	mapOfResults = map[int][]string{
 		2: {"(())", "()()"},
 	}
 
-	start := time.Now()
-	generateParentheses(n)
-	//runWithPrint(n)
-	elapsed := time.Since(start)
+	for i := 0; i < 4; i++ {
+		start := time.Now()
+		x := generateParenthesesNoAppend(n)
+		//generateParentheses(n)
+		printSet(x)
+		elapsed := time.Since(start)
+		fmt.Println("Set", i, elapsed)
+	}
 
-	fmt.Println("DONE ", elapsed)
+	fmt.Println("DONE")
 }
 
-func generateParentheses(n int) *[]string {
+func generateParenthesesNoAppend(n int) *[]string {
+	if n == 2 {
+		return &[]string{"(())", "()()"}
+	}
+
+	baseSet := *generateParenthesesNoAppend(n - 1)
+	baseLength := len(baseSet)
+
+	returnSet := make([]string, baseLength*3-1)
+
+	firstThird := returnSet[0:baseLength]
+	secondThird := returnSet[baseLength : baseLength*2]
+	lastThird := returnSet[baseLength*2:]
+
+	//append parentheses on the left for the first third
+	for i := range firstThird {
+		firstThird[i] = "[]" + baseSet[i]
+	}
+
+	//surround with parentheses for the second third
+	for i := range secondThird {
+		secondThird[i] = "<" + baseSet[i] + ">"
+	}
+
+	//append parentheses on the right for the last third
+	for i := range lastThird {
+		lastThird[i] = baseSet[i] + "{}"
+	}
+
+	return &returnSet
+}
+
+func generateParenthesesWithMap(n int) *[]string {
 	elem, ok := mapOfResults[n]
 	if ok {
+		fmt.Println("hit", n)
 		return &elem
 	}
 
-	baseSet := *generateParentheses(n - 1)
+	baseSet := *generateParenthesesWithMap(n - 1)
 
 	var returnSet []string
 
@@ -52,10 +89,47 @@ func generateParentheses(n int) *[]string {
 	return &returnSet
 }
 
+func generateParentheses(n int) *[]string {
+	if n == 2 {
+		return &[]string{"(())", "()()"}
+	}
+
+	baseSet := *generateParentheses(n - 1)
+
+	var returnSet []string
+
+	//append parentheses on the left
+	for _, str := range baseSet {
+		returnSet = append(returnSet, "[]"+str)
+	}
+
+	//surround with parentheses
+	for _, str := range baseSet {
+		returnSet = append(returnSet, "<"+str+">")
+	}
+
+	//append parentheses on the right
+	for i := 0; i < len(baseSet)-1; i++ {
+		returnSet = append(returnSet, baseSet[i]+"{}")
+	}
+
+	return &returnSet
+}
+
 //noinspection GoUnusedFunction
 func runWithPrint(n int) {
-	setOfParentheses := *generateParentheses(n)
-	for _, paren := range setOfParentheses {
-		fmt.Println(paren)
+	setOfParentheses := generateParentheses(n)
+	printSet(setOfParentheses)
+}
+
+func printSet(setOfParentheses *[]string) {
+	for _, str := range *setOfParentheses {
+		fmt.Println(str)
+	}
+}
+
+func printSetByte(setOfParentheses *[][]byte) {
+	for _, str := range *setOfParentheses {
+		fmt.Println(str)
 	}
 }
